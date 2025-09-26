5e07acd61b3630d5639724cd70d69006c6e39905
au <auau0197@gmail.com>
2025-09-26 15:06:40 +0700

    get user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,24 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly svc: UsersService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('staff')
   create(@Body() body: { name: string; email: string; password: string }) {
     return this.svc.create(body);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   list() {
     return this.svc.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   get(@Param('id') id: string) {
     return this.svc.findOne(Number(id));
